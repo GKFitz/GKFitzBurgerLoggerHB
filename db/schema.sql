050245648fb45be61b5f4e8b5480608608ad5d4a
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-05 04:23:33 -0400

    stated the db file structure and the schema

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+Create the `burgers_db`.
+Switch to or use the `burgers_db`.
+   
+Create table `burgers`
+    id auto incrementing int that serves as the primary key.
+    burger_name a string.
+    devoured a boolean.
