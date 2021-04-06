4946291016d31387fa3c1c226bffcb90e61088cd
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-06 13:12:29 -0400

    added devouredBurger event and fixed misspellings

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,8 @@
 const [undevouredList, devouredList]= document.querySelectorAll("ul");
+
 getBurgers();
 
+//Refreshes the view
 async function getBurgers(){
     const res = await fetch("/burgers");
     const json = await res.json();
@@ -16,21 +18,38 @@ async function getBurgers(){
     }
 }
 
-
+// working on the view
 function addUndevouredBurger(burger){
     const li = document.createElement("li");
     li.innerHTML= burger.burger_name + ` <button data-id="${burger.id}">Devour</button>` ;
-    devouredList.appendChild(li);
+    undevouredList.appendChild(li);
     li.querySelector("button").addEventListener("click", devourBurger);
 };
-function devourBurger(){
 
-};
-
+// working on the view
 function addDevouredBurger(burger){
     const li = document.createElement("li");
-    li.textcontent= burger.burger_name;
+    li.textContent= burger.burger_name;
     devouredList.appendChild(li);
 
 }
+
+// changes the model; api call
+async function devourBurger(event){
+    const id = event.target.dataset.id;
+    const res = await fetch("/burger/devour", { 
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({id})
+    });
+    const json = await res.json();
+    console.log(json)
+    getBurgers();
+
+
+
+
+};
 
